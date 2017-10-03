3d3b1323a46151c479ed27c8417d916f3a6225a3
mblaul <32026169+mblaul@users.noreply.github.com>
2017-10-03 10:50:00 -0400

    Adjusted to match our code

--- DND_Inventory_mng_Garret/src/dnd_inventory_mng/CSVHandling.java
+++ DND_Inventory_mng_Garret/src/dnd_inventory_mng/CSVHandling.java
@@ -6,9 +6,10 @@
 package dnd_inventory_mng;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -17,40 +18,61 @@ import java.io.IOException;
  */
 
 public class CSVHandling {
-    public static void main(String[] args) {
-
-        String appDir = System.getProperty("user.dir");
-        String csvFile = appDir + "/files/test.csv";
+   
+    //Delimiters used in the CSV file
+    private static final String COMMA_DELIMITER = ",";
+    
+    public static void main(String args[])
+    {
         BufferedReader br = null;
-        String line = "";
-        String cvsSplitBy = ",";
-
-        try {
-
-            br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
-
-                // use comma as separator
-                String[] readFromFile = line.split(cvsSplitBy);
-
-                System.out.println(readFromFile[0]);
-
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        try
+        {
+            //Reading the csv file
+            br = new BufferedReader(new FileReader("Items.csv"));
+            
+            //Create List for holding Items objects
+            List<Items> itemList = new ArrayList<Items>();
+            
+            String line = "";
+            //Read to skip the header
+            br.readLine();
+            //Reading from the second line
+            while ((line = br.readLine()) != null) 
+            {
+                String[] itemDetails = line.split(COMMA_DELIMITER);
+                
+                if(itemDetails.length > 0 )
+                {
+                    //Save the employee details in Items object
+                    Items item = new Items(Integer.parseInt(itemDetails[0]),
+                            itemDetails[1],itemDetails[2],
+                            Integer.parseInt(itemDetails[3]));
+                    itemList.add(item);
                 }
             }
+            
+            //Lets print the Items List
+            for(Items e : itemList)
+            {
+                System.out.println(e.getEmpId()+"   "+e.getFirstName()+"   "
+                		+e.getLastName()+"   "+e.getSalary());
+            }
         }
-
+        catch(Exception ee)
+        {
+            ee.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                br.close();
+            }
+            catch(IOException ie)
+            {
+                System.out.println("Error occured while closing the BufferedReader");
+                ie.printStackTrace();
+            }
+        }
     }
-    
 }
