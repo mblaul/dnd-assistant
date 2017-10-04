931dd95f1307c4d3e32598ee8cc20ed365d77868
mblaul <32026169+mblaul@users.noreply.github.com>
2017-10-04 19:53:22 -0400

    Recommit to fix screw up

--- DND_Inventory_mng_Garret/src/dnd_inventory_mng/CSVHandling.java
+++ DND_Inventory_mng_Garret/src/dnd_inventory_mng/CSVHandling.java
@@ -5,10 +5,12 @@
  */
 package dnd_inventory_mng;
 
-import java.io.FileNotFoundException;
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
+
 
 /**
  *
@@ -16,18 +18,66 @@ import java.util.Scanner;
  */
 
 public class CSVHandling {
-    public static void main(String[] args) throws FileNotFoundException {
-        
-        ArrayList<ArrayList<String>> itemValueList = new ArrayList<ArrayList<String>>();
-        
-        String appDir = System.getProperty("user.dir");
-        String csvFile = appDir + "/src/dnd_inventory_mng/files/test.csv";
-        
-        Scanner scanner = new Scanner(new File(csvFile));
-        scanner.useDelimiter(",");
-        while(scanner.hasNext()){
-            System.out.print(scanner.next()+",");
+   
+    //Delimiters used in the CSV file
+    private static final String COMMA_DELIMITER = ",";
+    
+    public void readFromCSV(){
+        BufferedReader br = null;
+        try
+        {
+            //Reading the csv file
+            
+            br = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/src/dnd_inventory_mng/files/test.csv"));
+            
+            //Create List for holding Item objects
+            List<Item> itemList = new ArrayList<Item>();
+            
+            String line = "";
+            //Read to skip the header
+            br.readLine();
+            //Reading from the second line
+            while ((line = br.readLine()) != null) 
+            {
+                String[] itemDetails = line.split(COMMA_DELIMITER);
+                
+                if(itemDetails.length > 0 )
+                {
+                    //Save the employee details in Item object
+                    Item itemTemp = new Item(Integer.parseInt(itemDetails[0]),
+                            itemDetails[1],Integer.parseInt(itemDetails[2]),
+                            Integer.parseInt(itemDetails[3]));
+                    itemList.add(itemTemp);
+                }
+            }
+            
+            //Lets print the Item List
+            for(Item e : itemList)
+            {
+                System.out.println(e.getItemID()+"   "+e.getName()+"   "
+                		+e.getCost()+"   "+e.getWeight());
+            }
         }
-        scanner.close();
+        catch(Exception ee)
+        {
+            ee.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                br.close();
+            }
+            catch(IOException ie)
+            {
+                System.out.println("Error occured while closing the BufferedReader");
+                ie.printStackTrace();
+            }
+        }
     }
-}
+    
+    public void writeToCSV(){
+        
+    }
+    
+}    
